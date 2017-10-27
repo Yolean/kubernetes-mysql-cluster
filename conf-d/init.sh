0d860bd16dbbb2f8931918a5e0adb46aa65d766c
Staffan Olsson <staffan@repos.se>
2017-10-28 00:56:56 +0200

    The image has an /etc/mysql/my.cnf, so use conf.d/ as intended

--- conf-d/init.sh
+++ conf-d/init.sh
@@ -5,10 +5,8 @@ HOST_ID=${HOSTNAME##*-}
 
 [ -z "$DATADIR" ] && exit "Missing DATADIR variable" && exit 1
 
-MYCNF=/etc/mysql/conf.d/galera.cnf
-
 if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
-  sed -i "s|#init#password=#init#|password=$MYSQL_ROOT_PASSWORD|" $MYCNF
+  sed -i "s|#init#password=#init#|password=$MYSQL_ROOT_PASSWORD|" /etc/mysql/conf.d/client.cnf
 fi
 
 # https://github.com/docker-library/mariadb/commit/f76084f0f9dc13f29cce48c727440eb79b4e92fa#diff-b0fa4b30392406b32de6b8ffe36e290dR80
@@ -16,7 +14,7 @@ if [ ! -d "$DATADIR/mysql" ]; then
   echo "No database in $DATADIR; configuring $POD_NAME for initial start"
 
   if [ $HOST_ID -eq 0 ]; then
-    sed -i 's|#init#wsrep_new_cluster=true#init#|wsrep_new_cluster=true|' $MYCNF
+    sed -i 's|#init#wsrep_new_cluster=true#init#|wsrep_new_cluster=true|' /etc/mysql/conf.d/galera.cnf
     # ... should log:
     #[Note] WSREP: 'wsrep-new-cluster' option used, bootstrapping the cluster
     #[Note] WSREP: Setting initial position to 00000000-0000-0000-0000-000000000000:-1
