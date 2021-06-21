b70d36c730bbc5f0a4720242966ce868cbbd8002
Staffan Olsson <staffan@repos.se>
2021-06-21 08:43:02 +0200

    Experiments with Parallel pod management

--- test.sh
+++ test.sh
@@ -13,11 +13,18 @@ k3d cluster create mysqltest --agents 3 --agents-memory 512M
 
 kubectl create namespace mysqltest
 
-kubectl -n mysqltest apply -k ./base
-sleep 1
-kubectl -n mysqltest wait --timeout=120s --for=condition=Ready pod/ystack-mariadb-galera-0
+# With >1 replicas this leads to split brain despite ./base/bootstrap-no.yaml
+#kubectl -n mysqltest apply -k ./base
 
-kubectl -n mysqltest exec -c mariadb-galera ystack-mariadb-galera-0 -- mysql -u root "-pTESTROOT" -N -e "SHOW STATUS LIKE 'wsrep_cluster_size';"
+kubectl -n mysqltest apply -k ./base-bootstrap
+kubectl -n mysqltest rollout status statefulset/ystack-mariadb-galera
+
+show_cluster_size
+
+kubectl -n mysqltest apply -k ./base
+kubectl -n mysqltest rollout status statefulset/ystack-mariadb-galera
+
+show_cluster_size
 
 kubectl -n mysqltest scale --replicas=2 statefulset/ystack-mariadb-galera
 kubectl -n mysqltest rollout status statefulset/ystack-mariadb-galera
@@ -44,5 +51,12 @@ sleep 1
 show_cluster_size
 kubectl -n mysqltest wait --for=condition=Ready --timeout=60s pod/ystack-mariadb-galera-1
 show_cluster_size
+
+echo "From zero we expect \"base\" to fail to start"
+
+kubectl -n mysqltest scale --replicas=0 statefulset/ystack-mariadb-galera
+kubectl -n mysqltest rollout status statefulset/ystack-mariadb-galera
+kubectl -n mysqltest apply -k ./base
+kubectl -n mysqltest rollout status statefulset/ystack-mariadb-galera
 
 k3d cluster delete mysqltest
